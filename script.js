1f4c06aa570c6bbe4c343a35453fad22ce2ff884
Anurag-bhatia <anuragbhatia.me@gmail.com>
2021-06-22 12:28:47 +0530

    updated details and projects

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Fronted Developer.", "Graphic Designer.", "Freelancer.", "Open Source Enthusiats", "Freelancer."],
+        strings: ["Fronted Developer.", "Designer.", "Freelancer.", "Open Source Enthusiats.", "Freelancer."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Fronted Developer.", "Graphic Designer.", "Freelancer.", "Open Source Enthusiats", "Freelancer."],
+        strings: ["Fronted Developer.", "Graphic Designer.", "Freelancer.", "Open Source Enthusiats.", "Freelancer."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
